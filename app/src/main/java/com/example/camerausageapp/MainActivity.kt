bc7f005e72ed85bd9a6d2011d68c57ec6d1ec0c1
Abhas Kumar <abhas1102@gmail.com>
2021-08-16 20:44:18 +0530

    Applied Check for Camera Permission Granted after clicking on button.

--- app/src/main/java/com/example/camerausageapp/MainActivity.kt
+++ app/src/main/java/com/example/camerausageapp/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.camerausageapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val CAMERA_PERMISSION_CODE = 1 //Permission code
+        private const val CAMERA_REQUEST_CODE = 2 // for intent ActivityForResult
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    btnCamera.setOnClickListener {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)==  //Checking for permission granted
+                PackageManager.PERMISSION_GRANTED){
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA_REQUEST_CODE) // We need to pass the intent we want to start and code
+        } else{
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE
+            )
+        }
+    }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            } else{
+                Toast.makeText(this,"You denied the permission",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
